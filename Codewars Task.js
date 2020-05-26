bd0d8ce880d4a77ed8ca1825d391d83bc6cbe954
AntonVelio <aveliccko@gmail.com>
2020-05-25 21:30:05 -0700

    Added solution

---
+++ Codewars Task.js
@@ -0,0 +1,10 @@
+function countInfinity(arr){
+    let count = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(isFinite(arr[i])) count++;
+    }
+    return count;
+}
+console.log(countInfinity([1/0, 1/0]));
+
+console.log(isFinite(1/0));
