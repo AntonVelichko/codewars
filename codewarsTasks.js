0f0a80351aad416ac90ca020f6a93a384633b85b
AntonVelio <aveliccko@gmail.com>
2020-06-17 23:04:09 -0700

    Added solution

--- codewarsTasks.js
+++ codewarsTasks.js
@@ -3,6 +3,45 @@
 
 
 
+// https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/solutions/javascript
+
+function firstNonConsecutive (arr) {
+    for(let i = 0; i < arr.length; i++){
+        if( arr[i] - arr[i-1] !== 1 && isNaN(arr[i-1]) === false ) return arr[i];
+    }
+    return null;
+}
+
+
+function firstNonConsecutive (arr) {
+    console.log(arr);
+    for(let i = 0; i < arr.length-1; i++){
+        if((arr[i+1] - arr[i]) !== 1) { return arr[i+1]}
+    }
+    return null;
+}
+
+
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+        if (arr[i] + 1 !== arr[i + 1]) {
+            return arr[i + 1]
+        }
+    }
+    return null
+}
+
+
+function firstNonConsecutive(arr){
+    var out = null;
+    for(var i = 1; i < arr.length; i++){
+        if((arr[i] - arr[i - 1]) != 1){
+            out = arr[i];
+            break;
+        }
+    }
+    return out;
+}
 
 
 
@@ -40,11 +79,7 @@ function points(games) {
 }
 
 
-/*
--------------------------------------------------
-||||||||||||||||||||||||||||||||||||||||||||||||||
--------------------------------------------------
-*/
+
 
 
 // https://www.codewars.com/kata/582c81d982a0a65424000201/solutions/javascript
