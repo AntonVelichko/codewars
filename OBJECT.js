5b6cf08e40a0f91a64b51aa27e5b91f36b0c4aea
AntonVelio <aveliccko@gmail.com>
2020-07-17 12:18:55 -0700

    Added solution

--- OBJECT.js
+++ OBJECT.js
@@ -38,6 +38,166 @@ for(let i in person){
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 
+function isObjectFull(obj) {
+    for (let key in obj){
+        if (obj.hasOwnProperty(key)){
+            return true;
+        }
+    }
+    return false;
+}
+
+let obj = {
+    name: "Bob",
+    age: undefined,
+    city: '',
+};
+
+console.log(isObjectFull(obj)); // true
+
+console.log(Object.keys(obj)); // ["name", "age", "city"]
+console.log(Object.values(obj)); // ["Bob", undefined, ""]
+console.log(Object.entries(obj)); // [["name", "Bob"], ["age", undefined], ["city", ""]]
+console.log(Object.keys(obj).length) // 3
+
+
+if (Object.keys(obj).length === 0){
+    console.log('Empty');
+} else {
+    console.log('Not empty');
+}
+
+//--------------------------------------------------
+
+let obj = {
+    a: 1,
+    b: 2,
+};
+for (let key in obj){
+    console.log(obj[key]);
+}
+
+for (let key in obj){
+    console.log(key);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Посчитать, сколько раз встречается каждая буква в строке
+function countLetters(str) {
+    str = str.toLowerCase();
+    str = str.replace(/ /g, '');
+    console.log(str);
+    let obj = {};
+    for (let i = 0; i <str.length; i++){
+        if (obj[str[i]]){
+            obj[str[i]]++;
+        } else {
+            obj[str[i]] = 1;
+        }
+    }
+    return obj;
+}
+
+console.log(countLetters('I like Javascript and Python'));
+
+//---------------------------------------------
+
+let obj = 'I like Javascript and Python';
+
+function countLetters(str) {
+    str = str.toLowerCase();
+    str = str.replace(/ /g, '');
+    let obj = {};
+    for (let char of str){
+        if (obj[char]){
+            obj[char]++;
+        } else {
+            obj[char] = 1;
+        }
+    }
+    return obj;
+}
+console.log(countLetters(obj));
+
+//--------------------------------------------
+
+function countLetters(str){
+    let obj = {};
+    let arr = str.replace(/\s/g, '').toLowerCase().split('');
+    arr.forEach(el => obj[el] = (obj[el] || 0) + 1);
+    return obj;
+}
+
+console.log(countLetters('I like Javascript and Python'));
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Найти количество различных слов в строке
+function countWords(text){
+    text = text.toLowerCase().split(' ')
+    console.log(text);
+    let words = {};
+    for (let el of text) {
+        if (words[el]){
+            words[el]++;
+        } else {
+            words[el] = 1;
+        }
+    }
+    return words;  //объект, содержащий слова и количество каждого слова
+    return Object.keys(words); // массив из различных слов без дубликатов
+    return Object.keys(words).length; //количество различных слов
+}
+console.log(countWords("Roses are red violets are blue roses and violets are nice"))
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Дана строка s = "five nine eight two four"
+// Найти среднее арифметическое чисел в вывести в виде строки(округлить до целого)
+
+function averageStr(str){
+    let digits = {
+        zero: 0,
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        nine: 9
+    }
+    let arr = str.split(' ');
+    console.log(arr);
+    let sum = 0;
+    for (let el of arr){
+        sum = sum + digits[el]
+    }
+    console.log(sum);
+    let average = Math.round(sum /arr.length);
+    console.log(average);
+
+    for (let i in digits){
+        if (digits[i] === average){
+            return i
+        }
+    }
+}
+let s = "five nine eight two four";
+console.log(averageStr(s));
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
 // Задание 7. В прошлом году создан объект person.
 // Если у него есть свойство age, добавить к значению свойства 1,
 // если свойства нет, создать это свойство со значением 20.
